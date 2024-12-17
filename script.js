4e9931c029b980bc9b40dcd97b40da242d0e5273
btousifar <bn.tousifar86@gmail.com>
2024-12-16 21:09:52 -0600

    create function to add the book to myLibrary array
    Created function addBookToLibrary to take some
    arguments and call the object constructor (Book),
    and used the push array method to add the new
    object to the array.
    
    [Ticket: 1]

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = readStatus;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, readStatus) {
   // do stuff here
+    let book = new Book(title, author, pages, readStatus);
+    myLibrary.push(book);
+    console.log(book)
+
 }
+
+addBookToLibrary('Hobbit', 'J.R.R', 295, 'not yet read');
+addBookToLibrary('Hobbit', 'J.R.R', 295, 'not yet read');
+addBookToLibrary('Hobbit', 'J.R.R', 295, 'not yet read');
+console.log(myLibrary)
